e7123018be5d065c84f6d2225e8dad2f6825fde2
Devon Kirby <devonlkirby@gmail.com>
2025-06-10 17:30:00 -0500

    Added first unit tests

--- src/test/java/com/devon/gradebook/model/GradebookTest.java
+++ src/test/java/com/devon/gradebook/model/GradebookTest.java
@@ -1,4 +1,20 @@
 package com.devon.gradebook.model;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GradebookTest {
+
+    @Test
+    void testAddAndFindStudent() {
+        Gradebook gradebook = new Gradebook();
+        gradebook.addStudent("Devon");
+
+        Optional<Student> result = gradebook.findStudentByName("Devon");
+        assertTrue(result.isPresent());
+        assertEquals("Devon", result.get().getName());
+    }
 }
--- src/test/java/com/devon/gradebook/model/StudentTest.java
+++ src/test/java/com/devon/gradebook/model/StudentTest.java
@@ -1,4 +1,16 @@
 package com.devon.gradebook.model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StudentTest {
+
+    @Test
+    void testAddGrade() {
+        Student student = new Student("Devon");
+        student.addGrade("Math", "A");
+
+        assertEquals("A", student.getCourseGrades().get("Math"));
+    }
 }
